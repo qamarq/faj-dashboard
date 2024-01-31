8e5ce5a4683d809737deb083b201c024200ab4a9
QamarQ <kamilm@you2.pl>
2024-01-31 19:12:16 +0100

    api

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export default auth((req) => {
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix) || nextUrl.pathname.startsWith("/api/webhook")
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
     const isAuthRoute = authRoutes.includes(nextUrl.pathname)
 
