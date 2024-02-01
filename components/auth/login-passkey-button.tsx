f0f4b570d2f77f7e1a1858d33628cf8a8db1f7e9
QamarQ <kamilm@you2.pl>
2024-02-01 18:05:37 +0100

    loading

--- components/auth/login-passkey-button.tsx
+++ components/auth/login-passkey-button.tsx
@@ -3,18 +3,24 @@
 import { Button } from "@nextui-org/react";
 import { signInWithPasskey } from "@teamhanko/passkeys-next-auth-provider/client";
 import { UserCogIcon } from "lucide-react";
+import { useState } from "react";
 
 const SignInWithPasskey = () => {
+    const [isPending, setIsPending] = useState(false)
+
     return (
         <Button 
-            onClick={() => signInWithPasskey({ 
-                tenantId: process.env.NEXT_PUBLIC_PASSKEYS_TENANT_ID!, 
-                callbackUrl: `${window.location.origin}/` 
+            onClick={() => {
+                setIsPending(true)
+                signInWithPasskey({ 
+                    tenantId: process.env.NEXT_PUBLIC_PASSKEYS_TENANT_ID!, 
+                    callbackUrl: `${window.location.origin}/` 
                 })
-            } 
+            }} 
             className="mt-4 w-full" color="default" variant="flat"
+            isLoading={isPending}
         > 
-            <UserCogIcon className="w-4 h-4 mr-2" /> 
+            {!isPending && <UserCogIcon className="w-4 h-4 mr-2" />} 
             Sign in with a passkey 
         </Button>
     )
